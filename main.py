7859a3de44154b9c38516da28b410513cb1eaf2f
Rajneesh Singh <63272969+rajneesh011@users.noreply.github.com>
2022-02-20 17:41:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,15 @@ print()
 f= cgi.FieldStorage()
 cmd = f.getvalue("x")
 
-print(subprocess.getoutput("sudo " + cmd))
+if("run" in cmd ) or ("launch" in cmd ): 
+    print(subprocess.getoutput("sudo docker run -d --name test centos:latest"))
+elif("stop" in cmd ): 
+    print(subprocess.getoutput("sudo docker stop test")
+elif("download" in cmd ) or ("pull" in cmd ):
+    print(subprocess.getoutput("sudo docker pull centos:latest)
+elif("terminate" in cmd ) or ("remove" in cmd):
+    print(subprocess.getoutput("sudo rm test") 
+elif("show" in cmd) or ("images" in cmd):
+    print(subprocess.getoutput("sudo docker images")
+else: 
+    print("please wait for this feature we are working on it :")
